fe35ce2b34c726ee18615be52080d45d755334b8
unknown <k.bhagya956@gmail.com>
2024-07-01 12:49:39 +0530

    added files

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/connectDB')
+const router = require('./routes/index')
+const cookiesParser = require('cookie-parser')
+const {app,server} = require('./socket/index')
+
+// const app = express();
+// app.use(cors({
+//     orgin : "*",
+//     credentials : true
+// }))
+
+app.use(cors({
+    origin: function (origin, callback) {
+        callback(null, true); // Allow any origin
+    },
+    credentials: true
+}));
+
+app.use(express.json())
+app.use(cookiesParser())
+
+const PORT = process.env.PORT || 8080
+
+app.get('/',(request,response)=>{
+    response.json({
+        message : "Server running at " + PORT
+    })
+})
+
+//api endpoints 
+app.use('/api',router)
+
+connectDB()
+server.listen(PORT,()=>{
+    console.log("server running at " + PORT)
+})
---
+++ server/models/UserModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : [true, "provide name"]
+    },
+    email : {
+        type :String,
+        required : [true,"provide email"],
+        unique : true
+    },
+    password : {
+        type :String,
+        required : [true,"provide password"]
+    },
+    profile_pic :{
+        type : String,
+        default : ""
+
+    }
+},{
+    timestamps : true
+
+})
+
+const User = mongoose.model('User',UserSchema)
+
+module.exports = User 
+
